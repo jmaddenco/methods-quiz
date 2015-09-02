a78ec9d066845f792d8da8a28b544a7b7dc5669d
Josh Madden <jmaddenco@gmail.com>
2015-09-02 17:27:59 -0600

    i finish everything the right way except for pig_latinity

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,43 +1,53 @@
 module MethodsQuiz
 	
-	# TODO - write has_teen?
-	def has_teen?(a,b,c)
-		if a>=13 && a<=19 || b>=13 && b<=19 || c>=13 && c<=19
-			10
-				return true
+	def has_teen?(a, b, c)
+		if a >= 13 && a <= 19
+			true
+		elsif b >= 13 && b <= 19
+			true
+		elsif c >= 13 && c <= 19
+			true
+		else
+			false
 		end
-		return false
+	end	
+	
+	def not_string(str)
+		if str.start_with?("not")
+			return "#{str}"
+		else
+			return "not#{str}"
+		end
 	end
 	
-	# TODO - write not_string
-	def not_string?(n)
-		if n= "not"
-			return #dont know what goes here
+	def icy_hot?(d, e)
+		if d > 100 && e < 0 || d < 0 && e > 100
+			true
+		else
+			false
 		end
+	end
+	
+	def closer_to(num, guess1, guess2)
+		if (num - guess1).abs == (num - guess2).abs
+			return 0
+		elsif (num - guess1).abs <= (num - guess2).abs
+			return (num - guess1).abs
+		elsif (num - guess1).abs >= (num - guess2).abs
+			return (num - guess2).abs
+		end
+	end
+
+	def two_as_one?(f, g, h)
+		if f + g == h || h + f == g || g + h == f
+			true
+		else
+			false
+		end
+	end
+
+	def pig_latinify()
 		
 	end
-	# TODO - write icy_hot?
-	def icy_hot?(atemp, btemp)
-		if atemp<=0 && btemp>=100
-			true
-		end
-
-	end
-	# TODO - write closer_to
-	def closer_to(guess1, guess2)
-		if guess1<guess2 || guess2>guess1
-
-			#dont know
-		end
-	end
-	# TODO - write two_as_one?
-	def two_as_one?(d, e, f)
-		if d + e == f || e + f == d || d + f == e
-			true
-			
-		end
-		
-	end
-	# TODO - write pig_latinify
 
 end
--- methods_quiz_test.rb
+++ methods_quiz_test.rb
@@ -11,40 +11,52 @@ class MethodsQuizTest < MiniTest::Test
    	end.new
 	end
 
-	# TODO - write tests here
-	def test_has_teen?
-		assert_equal true, @m.has_teen?(13,19,15)
-		assert_equal false, @m.has_teen?(2,3,5)
+	def test_has_teen
+		assert_equal true, @m.has_teen?(13, 5, 8)
+		assert_equal true, @m.has_teen?(1, 15, 21)
+		assert_equal true, @m.has_teen?(15, 15, 18)
+
+		assert_equal false, @m.has_teen?(5, 25, 8)
+		assert_equal false, @m.has_teen?(12, 2, 86)
 	end
+
 
 	def test_not_string
-		assert_equal "not", @m.not_string?("just another day on the farm")
-		assert_equal "not", @m.not_string?("not going to do this very well")
-		
-	end
+		assert_equal "notbag", @m.not_string("bag")
+		assert_equal "nothing", @m.not_string("nothing")
+	end	
 
 	def test_icy_hot?
-		assert_equal true, @m.icy_hot?(-2,191)
-		assert_equal true, @m.icy_hot?(0,100)
-		assert_equal true, @m.icy_hot?(-11,929)
+		assert_equal true, @m.icy_hot?(102, -1)
+		assert_equal true, @m.icy_hot?(-75, 250)
+
+		assert_equal false, @m.icy_hot?(0, 100)
+		assert_equal false, @m.icy_hot?(75, 25)
+		assert_equal false, @m.icy_hot?(750, 25)
+		assert_equal false, @m.icy_hot?(75, -25)
+		assert_equal false, @m.icy_hot?(5, 258)
+
+		assert_equal false, @m.icy_hot?(-75, 70)
 	end
 
 	def test_closer_to
-		assert_equal 0, @m.closer_to(0,10)
-		assert_equal 0, @m.closer_to(0,10)
+		assert_equal 0, @m.closer_to(0, -25, 25)	
+		assert_equal 23, @m.closer_to(0, 29, 23)	
+		assert_equal 2, @m.closer_to(0, 57, 2)	
+	end
+
+	def test_two_as_one
+		assert_equal true, @m.two_as_one?(1, 2, 3)
+		assert_equal true, @m.two_as_one?(4, 2, 2)
+		assert_equal true, @m.two_as_one?(4, 6, 2)
+
+		assert_equal false, @m.two_as_one?(1, 1, 1)
+		assert_equal false, @m.two_as_one?(1, 14, 21)
+		assert_equal false, @m.two_as_one?(666, 114, 221)
+	end
+	
+	def test_pig_latinify
 		
 	end
-
-	def test_two_as_one?
-		assert_equal true, @m.two_as_one?(10,10,20)
-		assert_equal true, @m.two_as_one?(30,10,20)
-		assert_equal true, @m.two_as_one?(30,50,20)
-	end
-
-	# def test_pig_latinify
-	# 	assert_equal "yas", @m.pig_latinigy("say")
-	# 	assert_equal "etaiverbbaay", @m.pig_latinigy("Abbreviate")
-	# 	assert_equal "etaiverbbaay", @m.pig_latinigy("Abbreviate")
-	# end
 
 end
